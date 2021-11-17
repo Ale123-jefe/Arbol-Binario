6cd65ce792a5d8f7d2e5cd5ea49bbbbc2abdd2d9
Ale123-jefe <83024502+Ale123-jefe@users.noreply.github.com>
2021-11-17 09:19:07 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import abb.ArbolBinarioBusqueda;
+
+public class Main {
+    public static void main(String[] args) {
+        ArbolBinarioBusqueda abb = new ArbolBinarioBusqueda();
+
+        //insertar elementos
+        abb.insertar(3);
+        abb.insertar(2);
+        abb.insertar(4);
+        abb.insertar(1);
+        abb.insertar(3);
+        abb.insertar(5);
+        abb.insertar(7);
+        abb.insertar(6);
+
+        //recoremos/mostrar los elementos del arbol inOrden
+        abb.mostrarInOrden();
+    }
+}
